0cb6b274e3866d7dd619cb8d6688cc5ef3fec0dc
Srivari Chettiyar <69836058+Srivari07@users.noreply.github.com>
2022-10-29 15:29:18 +0530

    Update disjointSet.py

--- graph/disjointSet.py
+++ graph/disjointSet.py
@@ -1,10 +1,12 @@
 class DisjointSet:
     rank=[]
     parent=[]
+    size=[]
 
     def __init__(self, n) -> None:
         self.rank=[0]*(n+1)
         self.parent=[i for i in range(0,n+1)]
+        self.size = [1]*(n+1)
 
     def findParent(self, node):
         if node==self.parent[node]:
@@ -13,7 +15,7 @@ class DisjointSet:
         self.parent[node]=self.findParent(self.parent[node])
         return self.parent[node]
 
-    def union(self,u,v):
+    def unionByRank(self,u,v):
         ulp_u=self.findParent(u)
         ulp_v=self.findParent(v)
 
@@ -27,11 +29,31 @@ class DisjointSet:
             self.parent[ulp_v] = ulp_u
             self.rank[ulp_u]+=1
 
+    def unionBySize(self, u, v):
+        ulp_u = self.findParent(u)
+        ulp_v = self.findParent(v)
+
+        if ulp_u==ulp_v:
+            return
+
+        if self.size[ulp_u] < self.size[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+            self.size[ulp_v] += self.size[ulp_u]
+
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.size[ulp_u] += self.size[ulp_v]
+
 obj=DisjointSet(5)
-obj.union(0, 2)
-obj.union(4, 2)
-obj.union(3, 1)
+# obj.unionByRank(0, 2)
+# obj.unionByRank(4, 2)
+# obj.unionByRank(3, 1)
+
+obj.unionBySize(0, 2)
+obj.unionBySize(4, 2)
+obj.unionBySize(3, 1)
 # print(obj.rank)
+# print(obj.size)
 # print(obj.parent)
 if obj.findParent(4) == obj.findParent(0):
     print('Yes')
@@ -41,4 +63,4 @@ if obj.findParent(1) == obj.findParent(0):
     print('Yes')
 else:
     print('No')
-        
+    
